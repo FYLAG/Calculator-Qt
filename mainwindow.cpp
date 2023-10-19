219ada8dc1be9f3db184e157a1e4ca4330e5399b
FYLAG <super.alex20030204@gmail.com>
2023-10-19 20:33:16 +0300

    The names of widgets have been changed and tracking of their clicks has been added.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,38 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow) {
 
     ui->setupUi(this);
+
+    connect(ui->pushButton_0, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_1, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_2, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_3, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_4, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_5, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_6, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_7, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_8, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+    connect(ui->pushButton_9, SIGNAL(clicked()), this, SLOT(eventClickNumber()));
+
+}
+
+void MainWindow::eventClickNumber() {
+
+    static double result = 0;
+
+    QPushButton *button = static_cast<QPushButton*>(sender());
+
+    result = result * 10 + (button->text()).toDouble();
+
+    QString resultStr = QString::number(result, 'g', 12);
+
+    ui->label_result->setText(resultStr);
 
 }
 
@@ -14,3 +41,4 @@ MainWindow::~MainWindow() {
     delete ui;
 
 }
+
--- mainwindow.h
+++ mainwindow.h
@@ -7,15 +7,20 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
+
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
+
     Ui::MainWindow *ui;
+
+private slots:
+    void eventClickNumber();
+
 };
 #endif // MAINWINDOW_H
